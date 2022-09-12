170634d52f8f4b7f6da7002d281501f55c3ce860
Ilsur <yuzlikeyevi@gmail.com>
2022-09-12 10:35:17 -0500

    Initial Schema model created

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    text: String,
+    timestamps: true,
+})
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    email: {type: String, unique: true, required: true},
+}, {
+    posts: [postSchema]
+}
+)
+
+const User = mongoose.model('User', userSchema)
+module.exports = User;
